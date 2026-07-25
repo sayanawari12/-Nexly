8a4886d4034b4787b26fc220d43d4dc136a9151f
Sayan-byte <sayanawari9@gmail.com>
2026-07-25 07:43:33 +0530

    restore(technology): recover original premium technology page

--- src/pages/MasterLanguagePage.jsx
+++ src/pages/MasterLanguagePage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import CLearningHub from './CLearningHub';
+import CppLearningHub from './CppLearningHub';
+import PythonLearningHub from './PythonLearningHub';
+import JavaLearningHub from './JavaLearningHub';
 import MasterLanguageTemplate from '../components/language/MasterLanguageTemplate';
 import { LANGUAGE_HUB_DATA } from '../data/languageData';
 
 const MasterLanguagePage = ({ defaultLang }) => {
   const { langId } = useParams();
   const targetId = (langId || defaultLang || 'c').toLowerCase();
-  
-  // Fallback to C if unknown language requested
-  const langData = LANGUAGE_HUB_DATA[targetId] || LANGUAGE_HUB_DATA.c;
 
-  return <MasterLanguageTemplate data={langData} />;
+  switch (targetId) {
+    case 'c':
+      return <CLearningHub />;
+    case 'cpp':
+    case 'cplusplus':
+      return <CppLearningHub />;
+    case 'python':
+      return <PythonLearningHub />;
+    case 'java':
+      return <JavaLearningHub />;
+    default:
+      const langData = LANGUAGE_HUB_DATA[targetId] || LANGUAGE_HUB_DATA.c;
+      return <MasterLanguageTemplate data={langData} />;
+  }
 };
 
 export default MasterLanguagePage;
